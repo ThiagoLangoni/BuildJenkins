ff20fd2b3ae65028cbb2db939a7b75a9c1be4180
Thiago Piroupo Langoni <loucoporbike@hotmail.com>
2019-12-18 23:04:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # default route
 @app.route('/')
 def index():
-    return 'Build via Jenkins via click a véi automatico'
+    return 'Build via Jenkins via click a mony'
 
 # function for responses
 def results():
