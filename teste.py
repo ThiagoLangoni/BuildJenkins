9058095baf6f955b201cec5d551c54fe8e598f16
Thiago Piroupo Langoni <loucoporbike@hotmail.com>
2019-12-18 22:42:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # default route
 @app.route('/')
 def index():
-    return 'Build via Jenkins via click PQP'
+    return 'Build via Jenkins via click a'
 
 # function for responses
 def results():
